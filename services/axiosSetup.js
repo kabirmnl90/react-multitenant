997cb0e3f51a712b8febdfc2fed23cd477b2859a
Kabir <kabirmnl90@gmail.com>
2024-10-22 12:37:08 +0200

    changed backend host

--- services/axiosSetup.js
+++ services/axiosSetup.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: `http://localhost:4000`,
+  baseURL: `https://react-multitenant-server.onrender.com:4000`,
 });
 
 export default instance;
